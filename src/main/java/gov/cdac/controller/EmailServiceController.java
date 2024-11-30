5c8c3cd171f0f1afca367af19722822fb57b4aa5
shanur <shanurjamadar4@gmail.com>
2024-11-30 15:50:05 +0530

    Commit added - 2

--- src/main/java/gov/cdac/controller/EmailServiceController.java
+++ src/main/java/gov/cdac/controller/EmailServiceController.java
@@ -93,7 +93,7 @@ public class EmailServiceController {
 	
 	@GetMapping("{reqType:icg|casb|afcat|icgOfficer}")
 	public ModelAndView emailHomePage(@PathVariable String reqType) {
-		//Mail Home fetched
+		//Mail Home fetched now
 		return new ModelAndView("MailHome").addAllObjects(mailServiceFactory.getPageData(reqType));
 	}
 	
